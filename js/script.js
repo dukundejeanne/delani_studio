898daaf3ef62f06e50e8e76876d49b109ad2b2ef
Jeannne Dukunde <dukundem90@gmail.com>
2019-07-26 12:10:41 +0200

    add hover,animation

--- js/script.js
+++ js/script.js
@@ -20,23 +20,100 @@ $(document).ready(function(){
     });
 });
 
-$(function(){
-    // $('#img1').hover(function(){
-    //     $(this).width(1.20 * $(this).width());
-    //     $(this).height(1.20 * $(this).height());
-    // },
-    // function(){
-    //     $(this).width(0.8 * $(this).width());
-    //     $(this).height(0.8 * $(this).height());
 
-    // });
+    $('.word1').hide();
+    $('#w1').animate({opacity:1});
 
     $('#img1').hover(function(){
         $(this).stop().animate({opacity:.4},200);
-        $('.').fadeIn();
+        $('.word1').fadeIn();
     },
+    function(){
         $(this).stop().animate({opacity:1},500)
-        $('.').fadeOut();
-    
+        $('.word1').fadeOut();      
+    });
+    // ------------------------------------------
 
+    $('.word2').hide();
+    $('#w2').animate({opacity:1});
+
+    $('#img2').hover(function(){
+        $(this).stop().animate({opacity:.4},200);
+        $('.word2').fadeIn();
+    },
+    function(){
+        $(this).stop().animate({opacity:1},500)
+        $('.word2').fadeOut();      
+    });
+// -----------------------------------------
+$('.word3').hide();
+$('#w3').animate({opacity:1});
+
+$('#img3').hover(function(){
+    $(this).stop().animate({opacity:.4},200);
+    $('.word3').fadeIn();
+},
+function(){
+    $(this).stop().animate({opacity:1},500)
+    $('.word3').fadeOut();      
+});
+//----------------------------------------- 
+$('.word4').hide();
+$('#w4').animate({opacity:1});
+
+$('#img4').hover(function(){
+    $(this).stop().animate({opacity:.4},200);
+    $('.word4').fadeIn();
+},
+function(){
+    $(this).stop().animate({opacity:1},500)
+    $('.word4').fadeOut();      
+});
+// -------------------------------------
+$('.word5').hide();
+$('#w5').animate({opacity:1});
+
+$('#img5').hover(function(){
+    $(this).stop().animate({opacity:.4},200);
+    $('.word5').fadeIn();
+},
+function(){
+    $(this).stop().animate({opacity:1},500)
+    $('.word5').fadeOut();      
+});
+// ------------------------------
+$('.word6').hide();
+$('#w6').animate({opacity:1});
+
+$('#img6').hover(function(){
+    $(this).stop().animate({opacity:.4},200);
+    $('.word6').fadeIn();
+},
+function(){
+    $(this).stop().animate({opacity:1},500)
+    $('.word6').fadeOut();      
+});
+// ---------------------------------
+$('.word7').hide();
+$('#w7').animate({opacity:1});
+
+$('#img7').hover(function(){
+    $(this).stop().animate({opacity:.4},200);
+    $('.word7').fadeIn();
+},
+function(){
+    $(this).stop().animate({opacity:1},500)
+    $('.word7').fadeOut();      
+});
+// ------------------------------
+$('.word8').hide();
+$('#w8').animate({opacity:1});
+
+$('#img8').hover(function(){
+    $(this).stop().animate({opacity:.4},200);
+    $('.word8').fadeIn();
+},
+function(){
+    $(this).stop().animate({opacity:1},500)
+    $('.word8').fadeOut();      
 });
